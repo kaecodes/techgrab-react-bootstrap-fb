32c34b6383e6ae188c51a5552cc4ef6fae9ac407
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-08 09:17:06 -0700

    Add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <footer className="bg-primary text-center p-4 text-light fixed-bottom">
+      &copy; {year} Tech Grab, LLC. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
