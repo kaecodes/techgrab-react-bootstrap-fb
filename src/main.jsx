71a8638993ff397773491b58501a387a5aec9a6a
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-09 15:24:05 -0700

    Update main file to provide the app with access to redux store

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 // Import our custom CSS
 import "../src/assets/scss/custom.scss";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
