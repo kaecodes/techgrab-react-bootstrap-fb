2396855ddb3d152d22c027e095baaef40c421f64
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-10 12:24:43 -0700

    Updating routing through index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,6 @@ export { default as Home } from "../pages/home/Home";
 export { default as Login } from "../pages/auth/Login";
 export { default as Register } from "../pages/auth/Register";
 export { default as Reset } from "../pages/auth/Reset";
+export { default as About } from "../pages/about/About";
+export { default as Contact } from "../pages/contact/Contact";
+export { default as Shop } from "../pages/shop/Shop";
