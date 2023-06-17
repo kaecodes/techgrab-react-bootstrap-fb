9193ee2a5b4c93b7450d52ca1133f676def8ce1c
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-17 10:57:58 -0700

    Add product to firebase

--- src/components/admin/addProduct/AddProduct.jsx
+++ src/components/admin/addProduct/AddProduct.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import { storage } from "../../../firebase/config";
+import { db, storage } from "../../../firebase/config";
 import { toast } from "react-toastify";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../loader/Loader";
 
 const category = [
   { id: 1, name: "Laptops" },
@@ -14,29 +17,33 @@ const category = [
   { id: 8, name: "Gaming" },
 ];
 
+const initialState = {
+  name: "",
+  imageURL: "",
+  price: 0,
+  category: "",
+  brand: "",
+  desc: "",
+};
+
 const AddProduct = () => {
-  const [product, setProduct] = useState({
-    name: "",
-    imageURL: "",
-    price: 0,
-    category: "",
-    brand: "",
-    desc: "",
-  });
+  const [product, setProduct] = useState({ ...initialState });
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setProduct({ ...product, [name]: value });
   };
   const handleImageChange = (e) => {
+    // Get file
     const file = e.target.files[0];
-
+    // Put file in storage
     const storageRef = ref(storage, `techgrab/${Date.now()}${file.name}`);
-
-    // Upload the file and metadata
+    // Execute upload file to storage
     const uploadTask = uploadBytesResumable(storageRef, file);
-
     // Monitor upload progress
     uploadTask.on(
       "state_changed",
@@ -48,10 +55,12 @@ const AddProduct = () => {
         setUploadProgress(progress);
       },
       (error) => {
-        toast.error(error.message);
+        toast.error(error.message); // Handle unsuccessful uploads
       },
       () => {
+        // Handle successful uploads on complete
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // Pushes the image URL to imageURL property of product object
           setProduct({ ...product, imageURL: downloadURL });
           toast.success("Image uploaded successfully!");
         });
@@ -61,130 +70,154 @@ const AddProduct = () => {
 
   const addProduct = (e) => {
     e.preventDefault();
-    console.log(product);
+    setIsLoading(true);
+
+    // Add a new document with a generated id
+    try {
+      addDoc(collection(db, "products"), {
+        name: product.name,
+        imageURL: product.imageURL,
+        price: Number(product.price),
+        category: product.category,
+        brand: product.brand,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setIsLoading(false);
+      setUploadProgress(0);
+      setProduct({ ...initialState });
+      toast.success("Product uploaded successfully!");
+      navigate("/admin/view-products");
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
   };
 
   return (
-    <div className="container-md px-4 py-2">
-      <h2 className="py-1 my-3 text-center text-success fw-bold">
-        Add New Product
-      </h2>
-      <form className="shadow p-3 rounded mb-3" onSubmit={addProduct}>
-        <div className="mb-3">
-          <label className="form-label">Product Name:</label>
-          <input
-            type="text"
-            placeholder="Product Name"
-            className="form-control"
-            required
-            name="name"
-            value={product.name}
-            onChange={(e) => handleInputChange(e)}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Product Image:</label>
-          <div className="border p-3 rounded">
-            <div className="mb-2 text-center">
-              {uploadProgress === 0 ? null : (
-                <div
-                  style={{ backgroundColor: "#a9a9a9" }}
-                  className="rounded-5"
-                >
+    <>
+      {isLoading && <Loader />}
+      <div className="container-md px-4 py-2">
+        <h2 className="py-1 my-3 text-center text-success fw-bold">
+          Add New Product
+        </h2>
+        <form className="shadow p-3 rounded mb-3" onSubmit={addProduct}>
+          <div className="mb-3">
+            <label className="form-label">Product Name:</label>
+            <input
+              type="text"
+              placeholder="Product Name"
+              className="form-control"
+              required
+              name="name"
+              value={product.name}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Product Image:</label>
+            <div className="border p-3 rounded">
+              <div className="mb-2 text-center">
+                {uploadProgress === 0 ? null : (
                   <div
-                    className="bg-primary text-light py-1 px-4 rounded-5"
-                    style={{ width: `${uploadProgress}%` }}
+                    style={{ backgroundColor: "#a9a9a9" }}
+                    className="rounded-5"
                   >
-                    <small>
-                      {uploadProgress < 100
-                        ? `${uploadProgress}%`
-                        : `${uploadProgress}%`}
-                    </small>
+                    <div
+                      className="bg-primary text-light py-1 px-4 rounded-5"
+                      style={{ width: `${uploadProgress}%` }}
+                    >
+                      <small>
+                        {uploadProgress < 100
+                          ? `${uploadProgress}%`
+                          : `${uploadProgress}%`}
+                      </small>
+                    </div>
                   </div>
-                </div>
+                )}
+              </div>
+              <input
+                type="file"
+                accept="image/*"
+                placeholder="Product Image"
+                className="form-control mb-2"
+                name="image"
+                onChange={(e) => handleImageChange(e)}
+              />
+              {product.imageURL === "" ? null : (
+                <input
+                  type="text"
+                  //
+                  placeholder="imageURL"
+                  className="form-control"
+                  name="imageURL"
+                  value={product.imageURL}
+                  disabled
+                />
               )}
             </div>
-            <input
-              type="file"
-              accept="image/*"
-              placeholder="Product Image"
-              className="form-control mb-2"
-              name="image"
-              onChange={(e) => handleImageChange(e)}
-            />
-            {product.imageURL === "" ? null : (
-              <input
-                type="text"
-                //
-                placeholder="imageURL"
-                className="form-control"
-                name="imageURL"
-                value={product.imageURL}
-                disabled
-              />
-            )}
           </div>
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Product Price:</label>
-          <input
-            type="number"
-            placeholder="Product Price"
-            className="form-control"
-            required
-            name="price"
-            value={product.price}
-            onChange={(e) => handleInputChange(e)}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Product Category:</label>
-          <select
-            required
-            name="category"
-            className="form-select"
-            value={product.category}
-            onChange={(e) => handleInputChange(e)}
-          >
-            <option value="" disabled>
-              Choose Product Category
-            </option>
-            {category.map((cat) => {
-              return (
-                <option key={cat.id} value={cat.name}>
-                  {cat.name}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Product Brand:</label>
-          <input
-            type="text"
-            placeholder="Product Brand"
-            className="form-control"
-            required
-            name="brand"
-            value={product.brand}
-            onChange={(e) => handleInputChange(e)}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Product Description:</label>
-          <textarea
-            name="desc"
-            className="form-control"
-            required
-            value={product.desc}
-            cols="30"
-            rows="10"
-            onChange={(e) => handleInputChange(e)}
-          ></textarea>
-        </div>
-        <button className="btn btn-primary">Save Product</button>
-      </form>
-    </div>
+          <div className="mb-3">
+            <label className="form-label">Product Price:</label>
+            <input
+              type="number"
+              placeholder="Product Price"
+              className="form-control"
+              required
+              name="price"
+              value={product.price}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Product Category:</label>
+            <select
+              required
+              name="category"
+              className="form-select"
+              value={product.category}
+              onChange={(e) => handleInputChange(e)}
+            >
+              <option value="" disabled>
+                Choose Product Category
+              </option>
+              {category.map((cat) => {
+                return (
+                  <option key={cat.id} value={cat.name}>
+                    {cat.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Product Brand:</label>
+            <input
+              type="text"
+              placeholder="Product Brand"
+              className="form-control"
+              required
+              name="brand"
+              value={product.brand}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Product Description:</label>
+            <textarea
+              name="desc"
+              className="form-control"
+              required
+              value={product.desc}
+              cols="30"
+              rows="10"
+              onChange={(e) => handleInputChange(e)}
+            ></textarea>
+          </div>
+          <button className="btn btn-primary">Save Product</button>
+        </form>
+      </div>
+    </>
   );
 };
 
