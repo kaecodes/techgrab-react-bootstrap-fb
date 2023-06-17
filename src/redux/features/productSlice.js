1e590bed4343e326a0a3fe101931d024ee4c9fe7
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-17 15:48:52 -0700

    Create product slice in redux

---
+++ src/redux/features/productSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    // Store products in redux
+    STORE_PRODUCTS: (state, action) => {
+      console.log(action.payload);
+    },
+  },
+});
+
+export const { STORE_PRODUCTS } = productSlice.actions;
+
+export const selectProducts = (state) => state.product.products;
+
+export default productSlice.reducer;
