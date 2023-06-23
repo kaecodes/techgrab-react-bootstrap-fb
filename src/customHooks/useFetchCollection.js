3f3c33cbed46cd963547bf02638e21bd4f86e233
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-23 12:54:22 -0700

    Create custom hook to get collection from database

---
+++ src/customHooks/useFetchCollection.js
@@ -0,0 +1,40 @@
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useState } from "react";
+
+const useFetchCollection = (collectionName) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Get collection from database
+  const getCollection = () => {
+    setIsLoading(true);
+
+    try {
+      // Get a reference of all the docs
+      const docRef = collection(db, collectionName);
+      // Query how data will be fetched from database
+      const q = query(docRef, orderBy("createdAt", "desc"));
+      // Snapshot handler will receive a new query snapshot every time the query results change
+      onSnapshot(q, (querySnapshot) => {
+        const allData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allData);
+        setIsLoading(false);
+        setData(allData);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getCollection();
+  }, []);
+
+  return { data, isLoading };
+};
+
+export default useFetchCollection;
