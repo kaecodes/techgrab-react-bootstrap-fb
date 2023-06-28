c547276e002f56a5d67d91cc78d602930a164159
Kaela Nguyen <kaedtn96@gmail.com>
2023-06-27 22:12:16 -0700

    Create structure of product details page

--- src/components/product/ProductDetails.jsx
+++ src/components/product/ProductDetails.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/config";
+import { toast } from "react-toastify";
+import spinnerImg from "../../assets/images/spinner.jpg";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -15,10 +17,14 @@ const ProductDetails = () => {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      // Create an id for the fetched document
+      const obj = {
+        id: id,
+        ...docSnap.data(),
+      };
+      setProduct(obj);
     } else {
-      // docSnap.data() will be undefined in this case
-      console.log("No such document!");
+      toast.error("Product Not Found.");
     }
   };
 
@@ -26,7 +32,48 @@ const ProductDetails = () => {
     getProduct();
   }, []);
 
-  return <div>ProductDetails</div>;
+  return (
+    <section>
+      <div className="container mb-8">
+        <h2>Product Details</h2>
+        <Link to="/shop">
+          <button className="btn btn-primary">&larr; Back to Shop</button>
+        </Link>
+        {product === null ? (
+          <img src={spinnerImg} alt="Loading..." style={{ width: "100px" }} />
+        ) : (
+          <>
+            <div>
+              <img src={product.imageURL} alt={product.name} />
+            </div>
+            <div>
+              <h3>{product.name}</h3>
+              <p>{`$${product.price}`}</p>
+              <p>{product.desc}</p>
+              <p>
+                <strong>SKU: </strong>
+                {product.id}
+              </p>
+              <p>
+                <strong>Brand: </strong>
+                {product.brand}
+              </p>
+              <div>
+                <button className="btn">-</button>
+                <p>
+                  <strong>1</strong>
+                </p>
+                <button className="btn">+</button>
+              </div>
+              <button className="btn btn-warning text-light">
+                Add To Cart
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
