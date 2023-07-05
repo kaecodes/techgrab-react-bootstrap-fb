d11c5246145a0aa630d3d1008d443aa615187e42
Kaela Nguyen <kaedtn96@gmail.com>
2023-07-05 14:39:08 -0700

    Update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import productReducer from "./features/productSlice";
+import filterReducer from "./features/filterSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   product: productReducer,
+  filter: filterReducer,
 });
 
 const store = configureStore({
