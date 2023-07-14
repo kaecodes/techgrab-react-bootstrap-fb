febf8a7a2a899dbc7e60124ac66e1031dadf2cc4
Kaela Nguyen <kaedtn96@gmail.com>
2023-07-14 12:36:44 -0700

    Create checkout slice and update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import authReducer from "./features/authSlice";
 import productReducer from "./features/productSlice";
 import filterReducer from "./features/filterSlice";
 import cartReducer from "./features/cartSlice";
+import checkoutReducer from "./features/checkoutSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   product: productReducer,
   filter: filterReducer,
   cart: cartReducer,
+  checkout: checkoutReducer,
 });
 
 const store = configureStore({
