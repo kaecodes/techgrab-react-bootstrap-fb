2bbfa7f5155c9722ec809c540cf430383b3d3a97
Kaela Nguyen <kaedtn96@gmail.com>
2023-07-15 10:25:06 -0700

    Checkout from backend

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CALCULATE_TOTAL,
+  CALCULATE_TOTAL_QUANTITY,
+  selectCartItems,
+  selectCartTotalAmount,
+} from "../../redux/features/cartSlice";
+import { selectEmail } from "../../redux/features/authSlice";
+import {
+  selectBillingAddress,
+  selectShippingAddress,
+} from "../../redux/features/checkoutSlice";
+import { toast } from "react-toastify";
+import CheckoutForm from "../../components/CheckoutForm";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const [clientSecret, setClientSecret] = useState("");
+  const [message, setMessage] = useState("Initializing Checkout...");
+
+  const cartItems = useSelector(selectCartItems);
+  const totalAmount = useSelector(selectCartTotalAmount);
+  const customerEmail = useSelector(selectEmail);
+
+  const shippingAddress = useSelector(selectShippingAddress);
+  const billingAddress = useSelector(selectBillingAddress);
+
+  const dispatch = useDispatch();
+
+  // Calculate total amount and total quantity in case user refreshes the page
+  useEffect(() => {
+    dispatch(CALCULATE_TOTAL_QUANTITY());
+    dispatch(CALCULATE_TOTAL());
+  }, [dispatch, cartItems]);
+
+  const description = `Tech Grab Payment: Email: ${customerEmail}, Amount: ${totalAmount}`;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:4242/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        items: cartItems,
+        userEmail: customerEmail,
+        shipping: shippingAddress,
+        billing: billingAddress,
+        description,
+      }),
+    })
+      // If response is not okay, then reject the promise
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then((data) => setClientSecret(data.clientSecret))
+      .catch((error) => {
+        setMessage("Failed to initialize checkout.");
+        toast.error("Something went wrong.");
+      });
+  }, [customerEmail, cartItems, description, shippingAddress, billingAddress]);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <>
+      <section>
+        <div className="container">{!clientSecret && <h3>{message}</h3>}</div>
+      </section>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </>
+  );
 };
 
 export default Checkout;
